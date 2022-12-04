35f62c487c6c834f96bfba3e6564ac1d9aba0650
FilipFG7 <80648662+FilipFG7@users.noreply.github.com>
2022-12-04 15:09:58 +0100

    Update DataTable.py

--- Pages/DataTable.py
+++ Pages/DataTable.py
@@ -4,14 +4,17 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 
 dash.register_page(__name__)
-
+#open csv file and adjust it
 df = pd.read_csv(r'C:\Users\Filip\Desktop\Codes\KYD-Dashboard\ifood_df.csv')
 df = df[['Income','MntWines','MntFruits','MntMeat','MntFish','MntSweet','MntGold','MntTotal']]
 df.insert(0, 'Cust', range(1, 1 + len(df))) 
 
+#page layout
 def layout():   
     return dbc.Container([
-        dcc.Markdown('# Data table with income slider', style={'textAlign':'center'}),        
+        dcc.Markdown('# Data table with income slider', style={'textAlign':'center'}),  
+        html.Hr(),  
+        #create datatable to display data     
         dash_table.DataTable(
             columns=[
                 {'name': 'Cust', 'id': 'Cust', 'type': 'numeric'},
@@ -31,18 +34,19 @@ def layout():
         ),        
         dbc.Row([
             dbc.Col([
+                #create an income range slider 
                 dcc.RangeSlider(0, 120000, 5000, value=[50000,60000], id='income_slider')
             ], width=11)    
         ])
     ])
-
+    
+#create callback that adjusts the datatable based on the slider
 @callback(
     Output('table', 'data'),
     Input('income_slider', 'value'), 
 )
+#function that upadtes the data
 def update_dropdown_options(income_v1):
     dff = df.copy()
-
     dff=dff[(df['Income']>=income_v1[0])&(df['Income']<=income_v1[1])]
-
     return dff.to_dict('records')
